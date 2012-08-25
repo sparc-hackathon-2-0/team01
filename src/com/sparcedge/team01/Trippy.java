7e0090f626043641fa9216fc4f6ac59b1deb27dc
Steve Woodruff <steve@n9oh.com>
2012-08-25 12:31:25 -0400

    team01 FTW

--- src/com/sparcedge/team01/Trippy.java
+++ src/com/sparcedge/team01/Trippy.java
@@ -1,8 +1,12 @@
 package com.sparcedge.team01;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
@@ -15,6 +19,10 @@ public class Trippy extends Activity {
     public static String TAG = "Trippy";
     public static Boolean debug = false;
     public static Context mContext;
+    Weather wx = null;
+    ProgressDialog progressDialog = null;
+    String woeid = "29466";
+
 
     /**
      * Called when the activity is first created.
@@ -24,6 +32,7 @@ public class Trippy extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         mContext = this;
+        wx = new Weather();
 
         // logic: if not logged in with facebook, show facebook button and wait until they click it
         // if already logged in - show screen for 1 second and then move to screen 2 (new trip)
@@ -33,10 +42,12 @@ public class Trippy extends Activity {
             hideSplashSoon(2000L);
         }
 
+        progressDialog = ProgressDialog.show(Trippy.this, "", "Getting Weather...", true);
+        new WxWOEIDTask().execute("29466");
     }
 
     public static void LOG(String s) {
-        LOG(false, s);
+        LOG(true, s);
     }
 
     public static void LOG(Boolean always, String s) {
@@ -66,5 +77,56 @@ public class Trippy extends Activity {
             }
         }
     };
+
+    void showOKAlertMsg(String title, String msg, final Boolean xfinish) {
+        LOG(title + ": " + msg);
+        AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(this);
+        dialogBuilder.setNeutralButton("OK",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                        if(xfinish) { finish(); }
+                    }
+                });
+
+        dialogBuilder.setTitle(title);
+        dialogBuilder.setMessage(msg);
+        try {
+            dialogBuilder.show();
+        } catch (Exception e) {
+            LOG("OOPS: I can't show a showOKAlertMsg dialog: " + e.getMessage());
+        }
+    }
+
+    class WxWOEIDTask extends AsyncTask<String, Void, Boolean> {
+        private Exception exception;
+
+        protected Boolean doInBackground(String... args) {
+            try {
+                woeid = wx.getWOEID(args[0]);
+            } catch (Exception e) {
+                this.exception = e;
+                return null;
+            }
+            return true;
+        }
+
+        protected void onPostExecute(Boolean result) {
+            try {
+                progressDialog.dismiss();
+            } catch (Exception e) {}
+            if(result == null) {
+                LOG("An error occurred: " + exception.getMessage());
+            }
+            else if(result) {
+                // we got it
+                LOG("WOEID: " + woeid);
+            }
+            else {
+
+                showOKAlertMsg("Whoops!", "Failed to fetch weather data: Try again.", false);
+            }
+        }
+    }
 
 }
--- src/com/sparcedge/team01/Weather.java
+++ src/com/sparcedge/team01/Weather.java
@@ -3,19 +3,32 @@ package com.sparcedge.team01;
 import android.app.Activity;
 import android.os.Bundle;
 import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.CookieStore;
 import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.protocol.ClientContext;
 import org.apache.http.impl.client.BasicCookieStore;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.protocol.BasicHttpContext;
+import org.apache.http.protocol.HTTP;
 import org.apache.http.protocol.HttpContext;
 
 import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URI;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+
 
 /**
  * Created with IntelliJ IDEA.
@@ -29,11 +42,16 @@ public class Weather extends Activity {
     protected static HttpContext localContext = new BasicHttpContext();
     protected static CookieStore cookieStore = new BasicCookieStore();
 
+    Weather() {
+        Trippy.LOG("Creating cookie store...");
+        localContext.setAttribute(ClientContext.COOKIE_STORE, cookieStore);
+    }
+
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
     }
 
-    private static String getWOEID(String place) {
+    public String getWOEID(String place) {
         String woeid = "";
 
         String URL = "http://query.yahooapis.com/v1/public/yql?q=select*from%20geo.places%20where%20text=%22" +
@@ -43,7 +61,33 @@ public class Weather extends Activity {
             BufferedReader in = null;
             HttpClient client = new DefaultHttpClient();
             HttpGet request = new HttpGet();
-            request.setURI(new URI("http://www.cocorahs.org/Login.aspx"));
+            request.setURI(new URI(URL));
+            HttpResponse response = client.execute(request, localContext);
+            in = new BufferedReader
+                    (new InputStreamReader(response.getEntity().getContent()));
+            StringBuffer sb = new StringBuffer("");
+            String line = "";
+            String NL = System.getProperty("line.separator");
+            while ((line = in.readLine()) != null) {
+                sb.append(line + NL);
+            }
+            in.close();
+
+            String page = sb.toString();
+            //<woeid>12770203</woeid>
+            woeid = findPattern(page, "<woeid>(\\d+)</", 1);
+        } catch (Exception e) { Trippy.LOG("Exception occurred while fetching WOEID: " + e.getMessage());}
+        return woeid;
+    }
+
+    public String getViewState() {
+        String vs = "";
+
+        try {
+            BufferedReader in = null;
+            HttpClient client = new DefaultHttpClient();
+            HttpGet request = new HttpGet();
+            request.setURI(new URI("http://www.google.com"));
             HttpResponse response = client.execute(request, localContext);
             in = new BufferedReader
                     (new InputStreamReader(response.getEntity().getContent()));
@@ -55,9 +99,10 @@ public class Weather extends Activity {
             }
             in.close();
             String page = sb.toString();
-            woeid = findPattern(page, "__VIEWSTATE\" value=\"(.*)\"", 1);
+            vs = findPattern(page, "__VIEWSTATE\" value=\"(.*)\"", 1);
+            vs = vs.split(" ")[0].replaceAll("\"", "");
         } catch (Exception e) { Trippy.LOG("Exception occurred while fetching viewState: " + e.getMessage());}
-        return woeid;
+        return vs;
     }
 
     public static String findPattern(String src, String pattern, int groupNum){

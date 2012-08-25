aa9e8c4acbb2800c149eb89fbcd36230712e34f2
Steve Woodruff <steve@n9oh.com>
2012-08-25 10:53:45 -0400

    Adding resources

--- src/com/sparcedge/team01/Splash.java
+++ src/com/sparcedge/team01/Splash.java
@@ -3,7 +3,7 @@ package com.sparcedge.team01;
 import android.app.Activity;
 import android.os.Bundle;
 
-public class Main extends Activity {
+public class Splash extends Activity {
     /**
      * Called when the activity is first created.
      */
@@ -11,5 +11,10 @@ public class Main extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        // logic: if not logged in with facebook, show facebook button and wait until they click it
+        // if already logged in - show screen for 1 second and then move to screen 2 (new trip)
+
+
     }
 }
